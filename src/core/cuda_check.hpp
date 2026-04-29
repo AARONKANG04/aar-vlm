ed72d9287f9693945604aaf5355ab9e996ecf834
Aaron Kang <aaron.h.kang@icloud.com>
2026-04-29 08:18:16 -0700

    fix: add line continuations to CUDA_CHECK macro

--- src/core/cuda_check.hpp
+++ src/core/cuda_check.hpp
@@ -5,10 +5,13 @@
 #include <stdexcept>
 #include <string>
 
-#define CUDA_CHECK(call) 
-    do {
-        cudaError_t _err = (call);
-        if (_err != cudaSuccess) {
-            throw std::runtime_error(std::string("CUDA error: ") + cudaGetErrorString(_err) + " at " + __FILE__ + ":" + std::to_string(__LINE__));
-        }
+#define CUDA_CHECK(call)                                                  \
+    do {                                                                  \
+        cudaError_t _err = (call);                                        \
+        if (_err != cudaSuccess) {                                        \
+            throw std::runtime_error(std::string("CUDA error: ") +        \
+                                     cudaGetErrorString(_err) + " at " +  \
+                                     __FILE__ + ":" +                     \
+                                     std::to_string(__LINE__));           \
+        }                                                                 \
     } while (0)
